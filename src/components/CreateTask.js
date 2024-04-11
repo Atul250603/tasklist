6f8fe0845409252960bd1b683abba8cfc00ce57d
Atul Goyal <atul250603@gmail.com>
2024-04-11 15:01:44 +0530

    fixed loading spinner

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -63,7 +63,8 @@ function CreateTask({tasks,setTasks,fetchAllTasks,user}){
                         <div className="modal-content text-dark">
                             <div className="modal-header">
                                 <h1 className="modal-title fs-5" id="exampleModalLabel">New Task</h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={()=>setshowSpinner(false)}></button>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={()=>{setshowSpinner(false); setTitle('');
+            setDescription('');}}></button>
                             </div>
                             <div className="modal-body">
                               <TaskForm title={title} setTitle={setTitle} description={description} setDescription={setDescription}/>
